6edf89b7c7c41ad5e0a3e9cdda607c4c203c8e82
Stephen Edwards <34760298+sventi555@users.noreply.github.com>
2022-07-16 18:31:48 -0400

    fix: downgrade nanoid for cjs support

--- packages/lib/jest.config.js
+++ packages/lib/jest.config.js
@@ -1,11 +1,5 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  preset: 'ts-jest/presets/js-with-ts',
+  preset: 'ts-jest',
   testEnvironment: 'node',
-  globals: {
-    'ts-jest': {
-      tsconfig: 'src/test/tsconfig.json',
-    },
-  },
-  transformIgnorePatterns: ['node_modules/(?!nanoid/.*)'],
 };
