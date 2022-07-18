1eaf5e0e029fa5b1ea7b7fed8c0c3096c93c7498
Stephen Edwards <34760298+sventi555@users.noreply.github.com>
2022-07-18 18:13:09 -0400

    feat(server): outline first draft of data model

--- packages/server/src/db/migrations/0001-init-tables.sql
+++ packages/server/src/db/migrations/0001-init-tables.sql
@@ -1,22 +1,50 @@
--- CREATE TABLE IF NOT EXISTS users (
---   id text PRIMARY KEY
--- );
-
--- CREATE TABLE IF NOT EXISTS saves (
---   id serial PRIMARY KEY,
---   user_id text NOT NULL REFERENCES users (id),
---   structure text NOT NULL,
---   time timestamptz
--- );
-
-CREATE TABLE IF NOT EXISTS big_yikes (
-  id serial PRIMARY KEY,
-  structure text NOT NULL,
-  hash text
+CREATE TABLE IF NOT EXISTS users (
+  id text PRIMARY KEY
 );
 
--- CREATE TABLE IF NOT EXISTS discoveries (
---   user_id text NOT NULL REFERENCES users (id),
---   big_yikes_id integer NOT NULL REFERENCES big_yikes (id),
---   time timestamptz
--- );
+CREATE TABLE IF NOT EXISTS block_types {
+  id serial PRIMARY KEY,
+  coords text NOT NULL,
+  color text NOT NULL
+}
+
+CREATE TABLE IF NOT EXISTS bins {
+  id serial PRIMARY KEY
+}
+
+CREATE TABLE IF NOT EXISTS bin_entries {
+  id serial PRIMARY KEY,
+  block_type_id integer NOT NULL REFERENCES block_types (id),
+  count integer,
+  bin_id integer NOT NULL REFERENCES bins (id)
+}
+
+CREATE TABLE IF NOT EXISTS structures {
+  id serial PRIMARY KEY,
+  hash text NOT NULL
+}
+
+CREATE TABLE IF NOT EXISTS blocks {
+  id serial PRIMARY KEY,
+  block_type_id integer NOT NULL REFERENCES block_types (id),
+  structure_id integer NOT NULL REFERENCES structures (id)
+}
+
+CREATE TABLE IF NOT EXISTS projects {
+  id serial PRIMARY KEY,
+  structure_id integer NOT NULL REFERENCES structures (id),
+  bin_id integer NOT NULL REFERENCES bins (id)
+}
+
+CREATE TABLE IF NOT EXISTS saves (
+  id serial PRIMARY KEY,
+  user_id text NOT NULL REFERENCES users (id),
+  project_id integer NOT NULL REFERENCES projects (id)
+);
+
+CREATE TABLE IF NOT EXISTS discoveries (
+  id serial PRIMARY KEY,
+  structure_id integer NOT NULL REFERENCES structures (id),
+  user_id text NOT NULL REFERENCES users (id),
+  time timestamptz
+);
