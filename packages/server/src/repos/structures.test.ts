48bae94098edec932e714ec0e6e13c4f5c73526a
Stephen Edwards <34760298+sventi555@users.noreply.github.com>
2022-07-24 16:14:10 -0400

    feat(server): adds structure repo

---
+++ packages/server/src/repos/structures.test.ts
@@ -0,0 +1,48 @@
+import { Block, Structure } from '@big-yikes/lib';
+import { query } from '../db';
+import { blockTypes } from '../test/data';
+import { BlockTypesRepo } from './block-types';
+import { StructuresRepo } from './structures';
+
+const blocks1 = [new Block(blockTypes['1x1'], { x: 0, y: 0 })];
+const structure1 = new Structure(blocks1);
+
+describe('StructuresRepo', () => {
+  let structureId: number;
+
+  beforeAll(async () => {
+    await BlockTypesRepo.add(blockTypes['1x1']);
+  });
+
+  beforeEach(async () => {
+    await query('DELETE FROM blocks');
+    await query('DELETE FROM structures');
+
+    structureId = await StructuresRepo.add(structure1);
+  });
+
+  it('should add a structure to the db', async () => {
+    const allStructures = await query('SELECT * FROM structures');
+    expect(allStructures.length).toEqual(1);
+
+    const addedStructure = allStructures[0];
+    expect(addedStructure.hash).toEqual(structure1.hash);
+  });
+
+  it('should return the id of a structure if it has already been added', async () => {
+    const addedStructureId = await StructuresRepo.add(structure1);
+    expect(addedStructureId).toEqual(structureId);
+  });
+
+  it('should check if an identical structure exists', async () => {
+    const structureId = await StructuresRepo.getId(structure1);
+    expect(structureId).not.toBeNull();
+
+    const structure = (
+      await query('SELECT * FROM structures WHERE id = $1', [
+        (structureId as number).toString(),
+      ])
+    )[0];
+    expect(structure.hash).toEqual(structure1.hash);
+  });
+});
---
+++ packages/server/src/repos/structures.ts
@@ -0,0 +1,65 @@
+import { Block, Structure } from '@big-yikes/lib';
+import { query } from '../db';
+import { BlocksRepo } from './blocks';
+
+export class StructuresRepo {
+  static async getId(structure: Structure) {
+    const structuresWithSameHash: { id: number }[] = await query(
+      'SELECT id FROM structures WHERE hash = $1',
+      [structure.hash]
+    );
+
+    if (!structuresWithSameHash.length) return null;
+
+    for (const structureWithSameHash of structuresWithSameHash) {
+      const blocks: {
+        position_x: number;
+        position_y: number;
+        block_type_id: number;
+        coordinates: string;
+        color: string;
+      }[] = await query(
+        `SELECT position_x, position_y, block_type_id, coordinates, color FROM blocks
+        JOIN block_types ON block_type_id = block_types.id
+        WHERE structure_id = $1`,
+        [structureWithSameHash.id.toString()]
+      );
+
+      const otherStructure = new Structure(
+        blocks.map((b) => {
+          return new Block(
+            {
+              id: b.block_type_id,
+              coordinates: JSON.parse(b.coordinates),
+              color: b.color,
+            },
+            { x: b.position_x, y: b.position_y }
+          );
+        })
+      );
+
+      if (structure.isEqual(otherStructure)) return structureWithSameHash.id;
+    }
+
+    return null;
+  }
+
+  static async add(structure: Structure) {
+    const alreadyExists = await StructuresRepo.getId(structure);
+    if (alreadyExists !== null) return alreadyExists;
+
+    const newStructure: { id: number } = (
+      await query('INSERT INTO structures (hash) VALUES ($1) RETURNING id', [
+        structure.hash,
+      ])
+    )[0];
+
+    const structureId = newStructure.id;
+
+    for (const block of structure.blocks) {
+      await BlocksRepo.add(block, structureId);
+    }
+
+    return structureId;
+  }
+}
