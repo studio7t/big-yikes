7db874a6d5826674f95a0a464a15e0c1d42a44f6
Stephen Edwards <34760298+sventi555@users.noreply.github.com>
2022-07-23 21:19:44 -0400

    fix(server): format db migration 0001 correctly

--- packages/server/src/db/migrations/0001-init-tables.sql
+++ packages/server/src/db/migrations/0001-init-tables.sql
@@ -2,41 +2,41 @@ CREATE TABLE IF NOT EXISTS users (
   id text PRIMARY KEY
 );
 
-CREATE TABLE IF NOT EXISTS block_types {
+CREATE TABLE IF NOT EXISTS block_types (
   id serial PRIMARY KEY,
   coords text NOT NULL,
   color text NOT NULL
-}
+);
 
-CREATE TABLE IF NOT EXISTS bins {
+CREATE TABLE IF NOT EXISTS bins (
   id serial PRIMARY KEY
-}
+);
 
-CREATE TABLE IF NOT EXISTS bin_entries {
+CREATE TABLE IF NOT EXISTS bin_entries (
   id serial PRIMARY KEY,
   block_type_id integer NOT NULL REFERENCES block_types (id),
   count integer,
   bin_id integer NOT NULL REFERENCES bins (id)
-}
+);
 
-CREATE TABLE IF NOT EXISTS structures {
+CREATE TABLE IF NOT EXISTS structures (
   id serial PRIMARY KEY,
   hash text NOT NULL
-}
+);
 
-CREATE TABLE IF NOT EXISTS blocks {
+CREATE TABLE IF NOT EXISTS blocks (
   id serial PRIMARY KEY,
   position_x integer NOT NULL,
   position_y integer NOT NULL,
   block_type_id integer NOT NULL REFERENCES block_types (id),
   structure_id integer NOT NULL REFERENCES structures (id)
-}
+);
 
-CREATE TABLE IF NOT EXISTS projects {
+CREATE TABLE IF NOT EXISTS projects (
   id serial PRIMARY KEY,
   structure_id integer NOT NULL REFERENCES structures (id),
   bin_id integer NOT NULL REFERENCES bins (id)
-}
+);
 
 CREATE TABLE IF NOT EXISTS saves (
   id serial PRIMARY KEY,
