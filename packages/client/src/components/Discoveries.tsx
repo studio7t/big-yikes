b91df4363b305f302333258498ccedd508216538
Stephen Edwards <34760298+sventi555@users.noreply.github.com>
2022-09-05 20:07:35 -0400

    feat: tell user they found a big yikes

--- packages/client/src/components/Discoveries.tsx
+++ packages/client/src/components/Discoveries.tsx
@@ -1,25 +1,29 @@
+import { Discovery } from '@big-yikes/lib';
 import { useDiscoveries } from '../hooks/discoveries';
 
 export const Discoveries = () => {
   const { isDiscovery, isAuthenticated, discoveries } = useDiscoveries();
 
   if (isDiscovery) {
-    if (isAuthenticated) {
-      return (
-        <ul>
-          {discoveries.map((discovery) => (
-            <li key={discovery.username}>
-              {discovery.username}: {new Date(discovery.time).toTimeString()}
-            </li>
-          ))}
-        </ul>
-      );
-    } else {
-      return (
-        <p>Congrats, you found a Big Yikes! Log in to submit your discovery.</p>
-      );
-    }
+    return (
+      <div>
+        <p>Congrats, you found a Big Yikes!</p>
+        {isAuthenticated ? <DiscoveriesList discoveries={discoveries} /> : null}
+      </div>
+    );
   }
 
   return null;
+};
+
+const DiscoveriesList = ({ discoveries }: { discoveries: Discovery[] }) => {
+  return (
+    <ul>
+      {discoveries.map((discovery) => (
+        <li key={discovery.username}>
+          {discovery.username}: {new Date(discovery.time).toTimeString()}
+        </li>
+      ))}
+    </ul>
+  );
 };
