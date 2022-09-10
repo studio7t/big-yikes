2f36dff6562798bea3f61b23feed4694874aeb95
Stephen Edwards <34760298+sventi555@users.noreply.github.com>
2022-09-10 13:08:28 -0400

    feat: automatically switch block types after running out

--- packages/client/src/stores/project.store.ts
+++ packages/client/src/stores/project.store.ts
@@ -1,4 +1,12 @@
-import { Bin, Block, defaultBin, growBounds, Structure } from '@big-yikes/lib';
+import {
+  Bin,
+  Block,
+  BlockTypeSlug,
+  blockTypeSlugs,
+  defaultBin,
+  growBounds,
+  Structure,
+} from '@big-yikes/lib';
 import create from 'zustand';
 import snapUrl from '../assets/snap.mp3';
 import { useTentativeStore } from './tentative.store';
@@ -6,6 +14,8 @@ import { useTentativeStore } from './tentative.store';
 interface ProjectState {
   bin: Bin;
   structure: Structure;
+  addToBin: (blockType: BlockTypeSlug, count?: number) => void;
+  removeFromBin: (blockType: BlockTypeSlug, count?: number) => void;
   addBlockIfValid: (block: Block) => boolean;
   removeBlockIfValid: (block: Block) => boolean;
 }
@@ -13,8 +23,32 @@ interface ProjectState {
 export const useProjectStore = create<ProjectState>((set, get) => ({
   bin: defaultBin,
   structure: new Structure([]),
+  addToBin: (blockType: BlockTypeSlug, count = 1) => {
+    const { bin } = get();
+    set({ bin: { ...bin, [blockType]: bin[blockType] + count } });
+  },
+  removeFromBin: (blockType: BlockTypeSlug, count = 1) => {
+    const { bin } = get();
+
+    if (bin[blockType] - count <= 0) {
+      const blockTypeIndex = blockTypeSlugs.findIndex(
+        (slug) => slug === blockType
+      );
+
+      const { setBlockType } = useTentativeStore.getState();
+      for (let i = 1; i < blockTypeSlugs.length; i++) {
+        const nextBlockType =
+          blockTypeSlugs[(blockTypeIndex + i) % blockTypeSlugs.length];
+        if (bin[nextBlockType] !== 0) {
+          setBlockType(nextBlockType);
+          break;
+        }
+      }
+    }
+    set({ bin: { ...bin, [blockType]: Math.max(bin[blockType] - count, 0) } });
+  },
   addBlockIfValid: (block: Block) => {
-    const bin = get().bin;
+    const { bin, removeFromBin } = get();
     if (bin[block.type] === 0) {
       return false;
     }
@@ -28,10 +62,8 @@ export const useProjectStore = create<ProjectState>((set, get) => ({
         block,
       ]);
 
-      set({
-        structure: resultingStructure,
-        bin: { ...bin, [block.type]: bin[block.type] - 1 },
-      });
+      set({ structure: resultingStructure });
+      removeFromBin(block.type);
 
       new Audio(snapUrl).play();
 
@@ -41,17 +73,14 @@ export const useProjectStore = create<ProjectState>((set, get) => ({
     return false;
   },
   removeBlockIfValid: (block: Block) => {
-    const bin = get().bin;
+    const { addToBin } = get();
     const structure = get().structure;
 
     const resultingStructure = new Structure(structure.getBlocksWithout(block));
 
     if (resultingStructure.isValid()) {
-      set({
-        structure: resultingStructure,
-        bin: { ...bin, [block.type]: bin[block.type] + 1 },
-      });
-
+      set({ structure: resultingStructure });
+      addToBin(block.type);
       return true;
     }
 
