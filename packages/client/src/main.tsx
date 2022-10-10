0134de9bb3d5ac8131cdd5b5125ce6534c0a3ba0
Stephen Edwards <34760298+sventi555@users.noreply.github.com>
2022-10-09 21:27:10 -0400

    fix: use import meta in dev mode and port 3000

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -1,15 +1,16 @@
 import { Auth0Provider } from '@auth0/auth0-react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import config from './config';
 import './index.css';
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <Auth0Provider
-    domain={import.meta.env.VITE_AUTH0_DOMAIN}
-    clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
+    domain={config.auth.auth0Domain}
+    clientId={config.auth.auth0ClientId}
     redirectUri={window.location.origin}
-    audience={import.meta.env.VITE_AUTH0_AUDIENCE}
+    audience={config.auth.auth0Audience}
   >
     <App />
   </Auth0Provider>
